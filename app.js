b870855136342f178ee549f939f2cdefa6d9c91f
GuRuGu <peter.k.nets@gmail.com>
2019-12-22 08:58:39 +0200

    Handle errors and add some middleware

--- app.js
+++ app.js
@@ -1,11 +1,28 @@
 const express = require("express");
+const morgan = require("morgan");
 
 const app = express();
+
+// Initialize env variables
+require("dotenv").config({ path: "process.env" });
+
+// Initialize middleware
+app.use(morgan("dev"));
+app.use(express.json());
 
 const PORT = process.env.PORT || 5000;
 
 // Routes
 app.use("/api/books", require("./routes/index"));
+
+// Handle 404 errors
+app.use((req, res, next) => {
+  res.status(404).json({ msg: "Not found!" });
+});
+// Handle all errors
+app.use((err, req, res) => {
+  res.status(err.status || 500).json({ error: err.message });
+});
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}...`);
